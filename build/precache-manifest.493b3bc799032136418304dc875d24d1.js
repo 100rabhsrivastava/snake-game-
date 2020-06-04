3053c06cd2a283e8f97d4ef5be71bc73799bcedf
saurabh srivastava <srivastava100rabh1142000@gmail.com>
2020-06-04 22:55:23 +0530

    game

---
+++ build/precache-manifest.493b3bc799032136418304dc875d24d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e94ed0a41b6db8e903eb09162e845d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "784949a5c788ff8271f0",
+    "url": "/static/css/main.12be35d6.chunk.css"
+  },
+  {
+    "revision": "296d58d764c632ca46da",
+    "url": "/static/js/2.d3336fad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3336fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784949a5c788ff8271f0",
+    "url": "/static/js/main.12758c18.chunk.js"
+  },
+  {
+    "revision": "08cdd3b62cb2c7c134b2",
+    "url": "/static/js/runtime-main.0ac8b321.js"
+  }
+]);
